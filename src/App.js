cf803d594fc570bf5ba31476c12b17adab76a735
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-17 17:55:38 -0500

    Remove boilder-plate code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
+import React from "react";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p class="border px-3 py-3">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 class="text-9xl">Welcome to My Portfolio</h1>
     </div>
   );
 }
