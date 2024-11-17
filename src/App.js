120707f5c8fd69bd1fe6fee6991e4691d9d26693
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-17 15:46:06 -0500

    Add tailwind css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p class=" py-3 text-black border px-3" >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
