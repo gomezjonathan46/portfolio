8c1418d74ea21f0c56ca927eccd24054bbc668c3
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-17 17:14:51 -0500

    Remove tailwind class for testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p class="border px-3 py-3 text-black">
+        <p class="border px-3 py-3">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
