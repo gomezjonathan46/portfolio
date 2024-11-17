03535759cc3a61fecff70dbc13312ced55264767
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-17 15:59:56 -0500

    Add code to host react app on github pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p class=" py-3 text-black border px-3" >
+        <p class=" py-3 text-black border px-3">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
