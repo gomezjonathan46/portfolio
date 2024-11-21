be4639f41f60688f39e95c239239eccc28cf8474
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-20 20:00:15 -0500

    Add compatible SVG files as components for daytime nighttime slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Sun from "./components/Sun.js";
+import Moon from "./components/Moon.js";
 
 function App() {
   return (
     <div>
-      <h1 class="text-9xl">Welcome to My Portfolio</h1>
+      <h1 className="text-9xl">Welcome to My Portfolio</h1>
+      <Sun/>
+      <Moon/>
     </div>
   );
 }
---
+++ src/components/Moon.js
@@ -0,0 +1,53 @@
+import * as React from "react";
+
+const Moon = (props) => (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="svg1"
+    width="187.411"
+    height="187.411"
+    version="1.1"
+    viewBox="0 0 49.586 49.586"
+  >
+    <defs id="defs1">
+      <clipPath id="clipPath4" clipPathUnits="userSpaceOnUse">
+        <path
+          id="lpe_path-effect4"
+          fill="#fff"
+          strokeWidth="0.265"
+          d="M30.014 28.492H89.6v59.586H30.014Zm45.759 16.22a24.793 24.793 0 0 0-28.807-17.848 24.793 24.793 0 0 0-19.707 27.568 24.793 24.793 0 0 0 26.21 21.48 24.793 24.793 0 0 0 23.16-24.739"
+          className="powerclip"
+        ></path>
+      </clipPath>
+    </defs>
+    <g
+      id="layer1"
+      fill="#18171e"
+      fillOpacity="1"
+      strokeWidth="0.265"
+      transform="translate(-35.014 -33.492)"
+    >
+      <path
+        id="path1"
+        d="M84.6 58.285a24.793 24.793 0 0 1-23.16 24.739 24.793 24.793 0 0 1-26.21-21.48 24.793 24.793 0 0 1 19.706-27.569 24.793 24.793 0 0 1 28.807 17.848"
+        clipPath="url(#clipPath4)"
+      ></path>
+      <path
+        id="path3"
+        d="m54.37 58.72-2.13-4.115-3.96-2.41 4.116-2.13 2.41-3.959 2.13 4.115 3.959 2.41-4.116 2.13z"
+      ></path>
+      <path
+        id="path3-0"
+        d="m52.195 55.893-1.27-2.584-2.427-1.548 2.584-1.27 1.548-2.428 1.27 2.584 2.428 1.548-2.584 1.27z"
+        transform="translate(10.986 8.663)"
+      ></path>
+      <path
+        id="path3-0-9"
+        d="m43.714 47.41-.846-1.546-1.547-.845 1.547-.846.846-1.547.846 1.547 1.546.846-1.546.845z"
+        transform="translate(4.948 15.966)"
+      ></path>
+    </g>
+  </svg>
+);
+
+export default Moon;
