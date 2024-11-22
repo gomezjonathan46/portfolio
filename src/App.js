b1ea847e02fa008a7e139ddfe306b73ac0b12804
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-22 07:03:45 -0500

    Restructure file layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Nav from "./components/Nav";
-
+import Nav from "./features/nav/Nav";
 
 function App() {
   return (
     <div>
       {/* <h1 className="text-9xl fleur-de-leah">Welcome to My Portfolio</h1> */}
-      <Nav/>
+      <Nav />
     </div>
   );
 }
