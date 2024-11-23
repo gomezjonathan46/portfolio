d607de1fcb38d0a0eaa73d5c72da235016d8bc24
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-23 09:28:52 -0500

    Make nav bar sections responsive

--- src/features/nav/Nav.js
+++ src/features/nav/Nav.js
@@ -1,26 +1,14 @@
 import DayNightToggle from "./components/DayNightToggle";
+import Sections from "./components/Sections";
 
 export default function Nav() {
   return (
     <div className="relative mt-3 flex items-center">
-      <button className="google-font z-10 text-nowrap text-3xl">
+      <button className="google-font z-10 text-nowrap text-3xl sm:text-4xl md:text-[2.87rem]">
         Jonathan Gomez
       </button>
       <DayNightToggle />
-      <ul className="z-10 ml-auto flex">
-        <li>
-          <button className="hover:underline">About</button>
-        </li>
-        <li className="ml-2">
-          <button className="hover:underline">Projects</button>
-        </li>
-        <li className="ml-2">
-          <button className="hover:underline">Experience</button>
-        </li>
-        <li className="ml-2">
-          <button className="hover:underline">Contact</button>
-        </li>
-      </ul>
+      <Sections/>
     </div>
   );
 }
