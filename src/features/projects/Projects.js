f96b92fe51f9fda3aa102635e3d8269a13f3589f
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-25 13:25:35 -0500

    Style projects card

--- src/features/projects/Projects.js
+++ src/features/projects/Projects.js
@@ -1,6 +1,6 @@
 export default function Projects() {
   return (
-    <div className="mx-[5%] mt-[15%] sm:mt-[5%]">
+    <div className="mx-[5%] mt-[15%] sm:mt-[5%] mb-10">
       <Project />
     </div>
   );
@@ -8,12 +8,18 @@ export default function Projects() {
 
 function Project() {
   return (
-    <div className="bg-white/50 w-56 rounded-md">
-      <div className="h-56">
-        <div className="bg-black/75 w-56 h-56 absolute"></div>
-        <img className="h-full w-auto" src={require("./assets/cw.jpg")} alt="" />
-      </div>
-      <p className="ml-2">hi</p>
+    <div className="w-80 rounded-md bg-white/50 shadow-md">
+      <a href="https://gomezjonathan46.github.io/Portfolio/">
+        <div className="h-56">
+          <div className="absolute h-56 w-80 rounded-t-md bg-black/75"></div>
+          <img
+            className="h-full w-auto rounded-t-md"
+            src={require(`./assets/${'cw'}.jpg`)}
+            alt=""
+          />
+        </div>
+        <p className="ml-2">hi</p>
+      </a>
     </div>
   );
 }
