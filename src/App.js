f4c756b958da491e82a5ac58ea4c093daedecab7
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-25 09:13:29 -0500

    Add Projects section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./features/nav/Nav";
 import About from "./features/about/About";
+import Projects from "./features/projects/Projects";
 
 function App() {
   return (
     <div className="font-size-on-mobile">
       <Nav />
       <About />
+      <Projects />
     </div>
   );
 }
---
+++ src/features/projects/Projects.js
@@ -0,0 +1,7 @@
+export default function Projects() {
+    return (
+        <div className="mx-[5%] mt-[5%]">
+            <p>Projects</p>
+        </div>
+    )
+}
