d839f355d55ede2643380a362841de7dd3be6be2
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-25 13:55:14 -0500

    Add hover effect to project cards

--- src/features/projects/Projects.js
+++ src/features/projects/Projects.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 export default function Projects() {
   return (
     <div className="mx-[5%] mb-10 mt-[15%] sm:mt-[5%]">
-      <Project 
-        link={"http://staging.brownstein.group/clients/career-wardrobe/website/"}
+      <Project
+        link={
+          "http://staging.brownstein.group/clients/career-wardrobe/website/"
+        }
         image={"cw"}
         title={"Career Wardrobe Website"}
         alt={"Career Wardrobe"}
@@ -12,11 +16,19 @@ export default function Projects() {
 }
 
 function Project({ link, image, title, alt }) {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <div className="w-80 rounded-md bg-white/50 shadow-md">
-      <a href={link} target="_blank">
+    <div
+      className="w-80 rounded-md bg-white/50 shadow-md"
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <a href={link} target="_blank" rel="noreferrer">
         <div className="h-56">
-          <div className="absolute h-56 w-80 rounded-t-md bg-black/75"></div>
+          <div
+            className={`absolute h-56 w-80 rounded-t-md bg-black/25 ${isHovering ? "block" : "hidden"}`}
+          ></div>
           <img
             className="h-full w-auto rounded-t-md"
             src={require(`./assets/${image}.jpg`)}
