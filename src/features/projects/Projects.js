82962e7243000b89f063b8b9b9b4f65c98d89144
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-25 13:37:02 -0500

    Add props to project component for customization when adding to parent component

--- src/features/projects/Projects.js
+++ src/features/projects/Projects.js
@@ -1,24 +1,29 @@
 export default function Projects() {
   return (
-    <div className="mx-[5%] mt-[15%] sm:mt-[5%] mb-10">
-      <Project />
+    <div className="mx-[5%] mb-10 mt-[15%] sm:mt-[5%]">
+      <Project 
+        link={"http://staging.brownstein.group/clients/career-wardrobe/website/"}
+        image={"cw"}
+        title={"Career Wardrobe Website"}
+        alt={"Career Wardrobe"}
+      />
     </div>
   );
 }
 
-function Project() {
+function Project({ link, image, title, alt }) {
   return (
     <div className="w-80 rounded-md bg-white/50 shadow-md">
-      <a href="https://gomezjonathan46.github.io/Portfolio/">
+      <a href={link} target="_blank">
         <div className="h-56">
           <div className="absolute h-56 w-80 rounded-t-md bg-black/75"></div>
           <img
             className="h-full w-auto rounded-t-md"
-            src={require(`./assets/${'cw'}.jpg`)}
-            alt=""
+            src={require(`./assets/${image}.jpg`)}
+            alt={alt}
           />
         </div>
-        <p className="ml-2">hi</p>
+        <p className="ml-2 py-1">{title}</p>
       </a>
     </div>
   );
