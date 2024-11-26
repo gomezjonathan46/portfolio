df1432fd1d51504a4d0ee65dbd5704342173866a
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-26 10:41:38 -0500

    Create header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import Nav from "./features/nav/Nav";
 import About from "./features/about/About";
 import Projects from "./features/projects/Projects";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="font-size-on-mobile">
       <Nav />
+      <Header 
+        text="Hello"
+      />
       <About />
       <Projects />
     </div>
