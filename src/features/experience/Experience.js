aed8bb5171a5d0b198a4ad8316f890dc489fb8a8
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2024-11-27 21:02:40 -0500

    Populate experience component

--- src/features/experience/Experience.js
+++ src/features/experience/Experience.js
@@ -1,7 +1,16 @@
 export default function Experience() {
-    return (
-        <div>
-            hi
+  return (
+    <div className="mx-[5%] mb-[10%]">
+      <div className="flex justify-between">
+        <p>Application Engineer - I</p>
+        <div className="flex items-center">
+          <p className="mr-7">2021 - 2022</p>
+          <div className="relative h-4 w-4">
+            <div className="absolute top-2/4 -translate-y-1/2 h-[1px] w-4 bg-black"></div>
+            <div className="absolute left-2/4 -translate-x-1/2 h-4 w-[1px] bg-black"></div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
